9cd314ed19b4eeb20ef450eb3807c4b181f474d7
ismailmzk7 <muhammadismailmarzuki7@gmail.com>
2024-02-19 01:23:41 +0700

    dart packages

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_packages/hello.dart';
+
+void main() {
+  print(sayHello("Ismail"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
